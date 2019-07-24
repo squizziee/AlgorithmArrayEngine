bbeb7c5c23fcfdd4a68021fe4bbfe687090b3359
Hill <Hill@DigitaLL>
2019-07-24 11:32:29 +0200

    Delete interface

--- pj/util/IterEngine.java
+++ pj/util/IterEngine.java
@@ -5,10 +5,10 @@ import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author <strong>Ivan Lyango</strong>
- * @version 1.0
+ * @version 0.3
  */
 
-public abstract class IterEngine implements Showable {
+public abstract class IterEngine {
 	
 	/**
 	 * <p>
@@ -24,7 +24,7 @@ public abstract class IterEngine implements Showable {
 	public static void randomFill(int[] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = arr[i] = ThreadLocalRandom.current().nextInt();
-		}
+		}	
 	}
 	
 	/**
@@ -132,18 +132,18 @@ public abstract class IterEngine implements Showable {
 	
 	public static void bubbleSort(int[] arr) {
 		int temp;
-		boolean isSorted = false;
-		while (!isSorted) { 	
-		    isSorted = true;
-		    for (int i = 0; i < arr.length - 1; i++) {
-			if (arr[i] > arr [i+1]) {
-			    isSorted = false;
-			    temp = arr[i+1];
-			    arr[i+1] = arr[i];
-			    arr[i] = temp;
-			}
-		    }  
-		}
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr [i+1]) {
+                    isSorted = false;
+                    temp = arr[i+1];
+                    arr[i+1] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
 	}
 	
 	/**
--- pj/util/Showable.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-public interface Showable {
-
-	void showInfo();
-	
-}
